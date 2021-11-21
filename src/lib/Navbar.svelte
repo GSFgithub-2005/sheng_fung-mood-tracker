6739d3f3546b1827c9042fc5d74f49acc583cfd2
GSFgithub-2005 <darrengohsf@gmail.com>
2021-11-21 10:35:20 +0000

    first commit

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-light" style="background-color: #98ACF8!important;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">My Mood Tracker 😸🤗</a>
+    </div>
+</nav>
