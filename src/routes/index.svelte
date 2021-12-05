2e46c8c3a649f126c0e77b28ccdb2d7dc0154944
GSFgithub-2005 <darrengohsf@gmail.com>
2021-12-05 09:55:40 +0000

    second_commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,16 @@
 <script>
     import Greeting from '$lib/Greeting.svelte';
     import Entry from '$lib/Entry.svelte';
-</script>
+    import EntryModal from '$lib/EntryModal.svelte';
+    import supabase from '$lib/db';
+    async function signOut() {
+   	 const { error } = await supabase.auth.signOut();
 
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
+<EntryModal />
 
 <Greeting />
 
@@ -10,7 +18,7 @@
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal" data-bs-target="#staticBackdrop"/>
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" data-bs-toggle="modal" data-bs-target="#newEntry"/>
     </div>
 
     <div class="list-group mb-3">
@@ -18,5 +26,10 @@
     	<Entry />
    	 
     </div>
+</section>
+
+<!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary"  on:click={signOut}>Logout</button>
 </section>
 
